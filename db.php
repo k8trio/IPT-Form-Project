6c0326de681a295d495e9c189f18dbe4584b3507
Nathalie <zenzenitsu2@gmail.com>
2026-02-15 11:00:18 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 $host = 'localhost';
-$port = '3307'; //change this if needed (3306)
+$port = '3306'; //change this if needed (3306)
 $db_name = 'cv_db';
 $username = 'root';
 $password = '';
